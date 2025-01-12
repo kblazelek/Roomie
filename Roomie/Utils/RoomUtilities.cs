1dfc92773f5af21f4bac01407af059ef63ad2903
krzysztofb <traxxstudios@gmail.com>
2025-01-12 23:20:12 +0000

    Make tables mobile friendly

---
+++ Roomie/Utils/RoomUtilities.cs
@@ -0,0 +1,15 @@
+﻿namespace Roomie.Utils
+{
+    public static class RoomUtilities
+    {
+        public static string GetFloorName(int floor)
+        {
+            return floor switch
+            {
+                0 => "ground floor",
+                1 => "first floor",
+                _ => $"{floor} floor"
+            };
+        }
+    }
+}
